72c48349915fdbf5ecaf0959c456306a4e302c94
jadsonteless <jtcostha@gmail.com>
2023-05-30 13:41:05 -0300

    addMobile

---
+++ mobile/App.tsx
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
+  return (
+    <View className="flex-1 items-center justify-center bg-zinc-900">
+      <Text className="font-alt text-6xl font-bold text-gray-50">
+        Rocketseat
+      </Text>
+      <StatusBar style="light" translucent />
+    </View>
+  )
+}
