b2b5779459464fa2f31067ac24bc4fe839537893
jadsonteless <jtcostha@gmail.com>
2023-05-31 02:21:14 -0300

    restrutura do mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
+
+// Corpo da page
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-gray-950">
+      <Text className="text-5xl font-bold text-zinc-50">Rocketseat</Text>
+      <StatusBar style="light" />
+    </View>
+  )
+}
---
+++ mobile/prettier.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  singleQuote: false, // Desativa o uso de aspas simples
+  semi: false, // Desativa o uso de ponto e vírgula
+  plugins: [require("prettier-plugin-tailwindcss")],
+}
