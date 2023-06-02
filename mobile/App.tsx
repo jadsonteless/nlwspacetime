50a62038bb3d955ad73ea2372e08941971ecf755
jadsonteless <jtcostha@gmail.com>
2023-06-01 23:48:47 -0300

    FinalizandoInterface1

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar'
 import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
+import { styled } from 'nativewind'
 
 import {
   useFonts,
@@ -11,8 +12,6 @@ import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 import imagemPng from './src/assets/bg-blur.png'
 import ComponentComSvg from './src/assets/stripes.svg'
 import ComponentComLogoSvg from './src/assets/nlw-spacetime-logo.svg'
-
-import { styled } from 'nativewind'
 
 const VarComSvg = styled(ComponentComSvg)
 const VarComLogoSvg = styled(ComponentComLogoSvg)
@@ -32,14 +31,13 @@ export default function App() {
   return (
     <ImageBackground
       source={imagemPng}
-      className="relative flex-1 items-center bg-gray-950 px-8"
+      className="relative flex-1 items-center bg-gray-950 px-8 py-10"
       imageStyle={{ position: 'absolute', left: '-100%' }}
     >
       <VarComSvg className="absolute left-2" />
 
       <View className="flex-1 items-center justify-center gap-6">
         <VarComLogoSvg />
-
         <View className="space-y-2">
           <Text className="text-center font-title text-2xl leading-tight text-gray-50">
             Sua cápsula do tempo
@@ -49,12 +47,19 @@ export default function App() {
             quiser) com o mundo!
           </Text>
         </View>
-        <TouchableOpacity className="rounded-full bg-green-500 px-5 py-3">
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-2"
+        >
           <Text className="font-alt text-sm uppercase text-black">
             Cadastrar lembranças
           </Text>
         </TouchableOpacity>
       </View>
+
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Feito com 💙 no NLW da Rocketseat
+      </Text>
 
       <StatusBar style="light" translucent />
     </ImageBackground>
