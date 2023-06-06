59f8e7f6f04311ac3d085944dca2758f255095ce
jadsonteless <jtcostha@gmail.com>
2023-06-06 11:01:11 -0300

    AdicionandoAltenticadorGit

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -11,9 +11,9 @@ export default function Home() {
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
         {/* Stripes or tracejado */}
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
-        {/* PERFIL USER or CAIXA 1 */}
+        {/* Sign In or Crie sua conta */}
         <a
-          href=""
+          href={`https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`}
           className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
         >
           {/* User bar or Barra do usuario */}
